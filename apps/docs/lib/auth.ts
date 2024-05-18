7afe402ccb95550ef5d44e638e3d18c6bc95895a
Naman Parashar <nparashar150@gmail.com>
2024-05-19 02:04:09 +0530

    auth(docs): get or create user on login

--- apps/docs/lib/auth.ts
+++ apps/docs/lib/auth.ts
@@ -1,3 +1,4 @@
+import { client, edgeql } from "@repo/db";
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -20,4 +21,62 @@ export const AuthOptions: NextAuthOptions = {
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        const isUser = await edgeql
+          .select(edgeql.Person, (Person) => ({
+            id: Person.id,
+            email: Person.email,
+            avatar: Person.avatar,
+            provider: Person.provider,
+            created_at: Person.created_at,
+            updated_at: Person.updated_at,
+            last_name: Person.last_name,
+            first_name: Person.first_name,
+            password: Person.password,
+            last_login: Person.last_login,
+            is_verified: Person.is_verified,
+            archived_at: Person.archived_at,
+            is_archived: Person.is_archived,
+            marketing_consent: Person.marketing_consent,
+            number_of_logins: Person.number_of_logins,
+          }))
+          .run(client);
+
+        if (isUser.length !== 0) {
+          const { ...rest } = isUser[0];
+          token = { ...token, ...rest };
+        } else {
+          if (!user.email) throw new Error("Email not found");
+
+          const newUserBody = {
+            password: "",
+            email: user.email,
+            marketing_consent: false,
+            avatar: user.image || "",
+            last_name: user.name || "",
+            first_name: user.name || "",
+          };
+
+          const newUser = await edgeql
+            .insert(edgeql.Person, newUserBody)
+            .run(client);
+
+          token = { ...token, ...newUserBody, id: newUser?.id };
+        }
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (token) {
+        session.user = { ...session.user, ...token };
+      }
+
+      return session;
+    },
+  },
 };
