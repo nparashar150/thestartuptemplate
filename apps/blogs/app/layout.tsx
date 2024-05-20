8a4ea135c54d9898284c781dbf3431957bafecb7
Naman Parashar <nparashar150@gmail.com>
2024-05-21 01:37:12 +0530

    meta(blog): update title

--- apps/blogs/app/layout.tsx
+++ apps/blogs/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Docs - The Startup Template",
+  title: "Blogs - The Startup Template",
   description: "The Startup tempalte",
 };
 
