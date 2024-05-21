cd10b485888cf7a04f679e74fa3d60716ffc754b
Naman Parashar <nparashar150@gmail.com>
2024-05-22 02:27:36 +0530

    style(website): hide unconfigured sections

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -3,18 +3,18 @@
 import About from "./components/About";
 import Cta from "./components/Cta";
 import FAQ from "./components/FAQ";
-import Features from "./components/Features";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import HowItWorks from "./components/HowItWorks";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
-import Pricing from "./components/Pricing";
 import ScrollToTop from "./components/ScrollToTop";
-import Services from "./components/Services";
 import Partners from "./components/Partners";
-import Team from "./components/Team";
-import Testimonials from "./components/Testimonials";
+// import Features from "./components/Features"; // Uncomment this line to enable the Pricing section
+// import Services from "./components/Services"; // Uncomment this line to enable the Pricing section
+// import Testimonials from "./components/Testimonials"; // Uncomment this line to enable the Pricing section
+// import Pricing from "./components/Pricing"; // Uncomment this line to enable the Pricing section
+// import Team from "./components/Team"; // Uncomment this line to enable the Team section
 
 const Page = () => {
   return (
@@ -24,12 +24,10 @@ const Page = () => {
       <Partners />
       <About />
       <HowItWorks />
-      <Features />
-      <Services />
+      {/* <Features /> */}
+      {/* <Services /> */}
       <Cta />
-      <Testimonials />
-      <Team />
-      <Pricing />
+      {/* <Testimonials /> */}
       <Newsletter />
       <FAQ />
       <Footer />
