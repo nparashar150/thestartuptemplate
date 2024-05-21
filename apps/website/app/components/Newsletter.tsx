e2be683e927dc451ecfa595fc5922fe336e93fd4
Naman Parashar <nparashar150@gmail.com>
2024-05-21 21:05:13 +0530

    config(website): add newsletter config

--- apps/website/app/components/Newsletter.tsx
+++ apps/website/app/components/Newsletter.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@repo/ui/components/ui/button";
 import { Input } from "@repo/ui/components/ui/input";
+import { NEWSLETTER_CONFIG } from "../../config";
 
 const Newsletter = () => {
   const handleSubmit = (e: any) => {
@@ -11,26 +12,11 @@ const Newsletter = () => {
     <section id="newsletter">
       <hr className="w-11/12 mx-auto" />
       <div className="container py-24 sm:py-32">
-        <h3 className="text-center text-4xl md:text-5xl font-bold">
-          Join Our Daily{" "}
-          <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-            Newsletter
-          </span>
-        </h3>
-        <p className="text-xl text-muted-foreground text-center mt-4 mb-8">
-          Lorem ipsum dolor sit amet consectetur.
-        </p>
-
-        <form
-          className="flex flex-col w-full md:flex-row md:w-6/12 lg:w-4/12 mx-auto gap-4 md:gap-2"
-          onSubmit={handleSubmit}
-        >
-          <Input
-            placeholder="leomirandadev@gmail.com"
-            className="bg-muted/50 dark:bg-muted/80 "
-            aria-label="email"
-          />
-          <Button>Subscribe</Button>
+        <h3 className="text-center text-4xl md:text-5xl font-bold bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">{NEWSLETTER_CONFIG?.title}</h3>
+        <p className="text-xl text-muted-foreground text-center mt-4 mb-8">{NEWSLETTER_CONFIG?.subtitle}</p>
+        <form className="flex flex-col w-full md:flex-row md:w-6/12 lg:w-4/12 mx-auto gap-4 md:gap-2" onSubmit={handleSubmit}>
+          <Input placeholder={NEWSLETTER_CONFIG?.inputPlaceholder} className="bg-muted/50 dark:bg-muted/80 " aria-label="email" />
+          <Button>{NEWSLETTER_CONFIG?.buttonText}</Button>
         </form>
       </div>
 
--- apps/website/config.ts
+++ apps/website/config.ts
@@ -1,4 +1,11 @@
-import { FrequentlyAskedQuestions } from "./types";
+import { FrequentlyAskedQuestions, Newsletter } from "./types";
+
+const NEWSLETTER_CONFIG: Newsletter = {
+  buttonText: "Subscribe",
+  title: "Join my newsletter",
+  inputPlaceholder: "pro.dev@gmail.com",
+  subtitle: "Get occasional updates, tips, and tricks (I promise not to spam you, I'm too lazy for that)",
+};
 
 const FREQUENTLY_ASKED_QUESTIONS: FrequentlyAskedQuestions = {
   title: "Frequently Asked Questions",
@@ -8,29 +15,31 @@ const FREQUENTLY_ASKED_QUESTIONS: FrequentlyAskedQuestions = {
     {
       key: "item-1",
       question: "What is The Startup Template?",
-      answer: "The Startup Template is a pre-built template using EdgeDB, Next.js, ShadcnUI, and NextAuth to help you quickly set up and launch your web app. It includes a beautiful landing page, a dashboard, and essential integrations, saving you time and effort."
+      answer:
+        "The Startup Template is a pre-built template using EdgeDB, Next.js, ShadcnUI, and NextAuth to help you quickly set up and launch your web app. It includes a beautiful landing page, a dashboard, and essential integrations, saving you time and effort.",
     },
     {
       key: "item-2",
       question: "Do you offer support if I run into issues?",
-      answer: "If you have any questions or need assistance, feel free to reach out to me at nparashar150@gmail.com. We're here to help you get the most out of The Startup Template."
+      answer: "If you have any questions or need assistance, feel free to reach out to me at nparashar150@gmail.com. We're here to help you get the most out of The Startup Template.",
     },
     {
       key: "item-3",
       question: "How can I support the development of The Startup Template?",
-      answer: "If you find this project helpful, please consider supporting our work by donating https://github.com/sponsors/nparashar150. Your support helps us continue to improve and maintain the template."
+      answer: "If you find this project helpful, please consider supporting our work by donating https://github.com/sponsors/nparashar150. Your support helps us continue to improve and maintain the template.",
     },
     {
       key: "item-4",
       question: "Where can I find the documentation?",
-      answer: "Comprehensive documentation is available here. It includes guides on getting started, customization options, and more."
+      answer: "Comprehensive documentation is available here. It includes guides on getting started, customization options, and more.",
     },
     {
       key: "item-5",
       question: "Can I use The Startup Template for commercial projects?",
-      answer: "Yes, you can use The Startup Template for both personal and commercial projects. It's designed to help you launch your web app quickly, whether it's a hobby project or a business venture. Provided you mention the original authors."
+      answer:
+        "Yes, you can use The Startup Template for both personal and commercial projects. It's designed to help you launch your web app quickly, whether it's a hobby project or a business venture. Provided you mention the original authors.",
     },
-  ]
-}
+  ],
+};
 
-export { FREQUENTLY_ASKED_QUESTIONS };
+export { NEWSLETTER_CONFIG, FREQUENTLY_ASKED_QUESTIONS };
