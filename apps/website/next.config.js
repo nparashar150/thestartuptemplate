bdbc343e5d3d09663c602f250fc57c65b50c2409
Naman Parashar <nparashar150@gmail.com>
2024-05-21 15:49:31 +0530

    config(env): add blog url env

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const { DOCS_URL } = process.env;
+const { BLOGS_URL } = process.env;
+const LOCAL_BLOGS_URL = "http://localhost:3001";
 
 module.exports = {
   transpilePackages: ["@repo/ui"],
@@ -21,11 +22,11 @@ module.exports = {
       },
       {
         source: "/blogs",
-        destination: `${DOCS_URL}/blogs`,
+        destination: `${BLOGS_URL || LOCAL_BLOGS_URL}/blogs`,
       },
       {
         source: "/blogs/:path*",
-        destination: `${DOCS_URL}/blogs/:path*`,
+        destination: `${BLOGS_URL || LOCAL_BLOGS_URL}/blogs/:path*`,
       },
     ];
   },
