bbe36c0e624b9bc764b0a7204d59f2bf2d6401ee
Naman Parashar <nparashar150@gmail.com>
2024-05-23 04:22:05 +0530

    db(website): add get & set config

--- apps/website/lib/db.ts
+++ apps/website/lib/db.ts
@@ -1,4 +1,5 @@
 import { client, edgeql } from "@repo/db";
+import * as config from "../config";
 
 const getOrCreateUser = async (user: GetOrCreateUserProps) => {
   const person = await edgeql
@@ -17,9 +18,16 @@ const getOrCreateUser = async (user: GetOrCreateUserProps) => {
       last_name: user.name.split(" ").slice(1).join(" ") || "",
     };
 
+    await edgeql.insert(edgeql.Person, newUserBody).unlessConflict().run(client);
+
     await edgeql
-      .insert(edgeql.Person, newUserBody)
-      .unlessConflict()
+      .insert(edgeql.TemplateConfig, {
+        content: config,
+        creator: edgeql.select(edgeql.Person, (Person) => ({
+          ...edgeql.Person["*"],
+          filter_single: edgeql.op(Person.email, "=", user.email),
+        })),
+      })
       .run(client);
 
     const person = await edgeql
@@ -35,13 +43,41 @@ const getOrCreateUser = async (user: GetOrCreateUserProps) => {
   return { ...person, is_new: false };
 };
 
-
 const subscribeToNewsletter = async (email: string) => {
   const newsletterSubscriber = await edgeql.insert(edgeql.NewsletterSubscription, { email }).unlessConflict().run(client);
   return newsletterSubscriber;
 };
 
-export { getOrCreateUser, subscribeToNewsletter };
+const getTemplateConfig = async (email: string) => {
+  const templateConfig = await edgeql
+    .select(edgeql.TemplateConfig, (templateConfig) => ({
+      ...edgeql.TemplateConfig["*"],
+      filter_single: edgeql.op(templateConfig.creator.email, "=", email),
+    }))
+    .run(client);
+
+  return templateConfig;
+};
+
+const saveTemplateConfig = async (email: string, config: string) => {
+  await edgeql
+    .update(edgeql.TemplateConfig, (templateConfig) => ({
+      filter_single: edgeql.op(templateConfig.creator.email, "=", email),
+      set: { content: config },
+    }))
+    .run(client);
+
+  const newTemplateConfig = await edgeql
+    .select(edgeql.TemplateConfig, (templateConfig) => ({
+      ...edgeql.TemplateConfig["*"],
+      filter_single: edgeql.op(templateConfig.creator.email, "=", email),
+    }))
+    .run(client);
+
+  return newTemplateConfig;
+};
+
+export { getOrCreateUser, subscribeToNewsletter, getTemplateConfig, saveTemplateConfig };
 
 interface GetOrCreateUserProps {
   email: string;
