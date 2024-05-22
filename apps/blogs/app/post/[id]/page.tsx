823327276c7dbea53fcaf2884c4ce8b952520936
Naman Parashar <nparashar150@gmail.com>
2024-05-22 19:28:47 +0530

    style(blog): update blog markdown styles

--- apps/blogs/app/post/[id]/page.tsx
+++ apps/blogs/app/post/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { remark } from "remark";
+import html from "remark-html";
 import { client, edgeql } from "@repo/db";
 import Link from "next/link";
 
@@ -11,19 +13,22 @@ export default async function Post({ params }: { params: { id: string } }) {
     }))
     .run(client);
 
+  const processedContent = await remark().use(html).process(post?.content);
+  const contentHtml = processedContent.toString();
+
   if (!post) {
     return <div>Post not found</div>;
   }
 
   return (
-    <div className="container mx-auto p-4 bg-black text-white">
+    <div className="container mx-auto p-4 bg-gray-800 text-white">
       <nav>
         <Link href="/" className="text-blue-500 mb-4 block" replace>
           Back to list
         </Link>
       </nav>
       <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
-      <p>{post.content}</p>
+      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
     </div>
   );
 }
