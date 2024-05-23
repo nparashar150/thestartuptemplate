8db6d34508ae649b7259261a621126dc7766671a
Naman Parashar <nparashar150@gmail.com>
2024-05-24 02:59:02 +0530

    config(website): update login page conf

--- apps/website/app/(auth)/login/page.tsx
+++ apps/website/app/(auth)/login/page.tsx
@@ -1,25 +1,12 @@
 import GridPattern from "@repo/ui/components/grid-pattern";
 import { Icons } from "@repo/ui/components/icons";
-import { buttonVariants } from "@repo/ui/components/ui/button";
 import { cn } from "@repo/ui/lib/utils";
 import Link from "next/link";
-import {
-  GithubSignInButton,
-  GoogleSignInButton,
-} from "../../components/auth-button";
+import { GithubSignInButton, GoogleSignInButton } from "../../components/auth-button";
 
 export default function AuthenticationPage() {
   return (
     <div className="relative h-screen overflow-hidden flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-      <Link
-        href="/examples/authentication"
-        className={cn(
-          buttonVariants({ variant: "ghost" }),
-          "absolute right-4 hidden top-4 md:right-8 md:top-8",
-        )}
-      >
-        Login
-      </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900">
           <GridPattern
@@ -31,40 +18,25 @@ export default function AuthenticationPage() {
               [10, 5],
               [13, 3],
             ]}
-            className={cn(
-              "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
-              "inset-x-0 inset-y-[-50%] h-[200%] -skew-y-12",
-            )}
+            className={cn("[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]", "inset-x-0 inset-y-[-50%] h-[200%] -skew-y-12")}
           />
         </div>
-        <Link
-          href="/"
-          className="relative z-20 flex items-center text-lg font-medium"
-        >
+        <Link href="/" className="relative z-20 flex items-center text-lg font-medium">
           <Icons.logo className="h-6 w-6 mr-2" />
           The Startup
         </Link>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
-            <p className="text-lg">
-              &ldquo;Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vivamus commodo, erat ac egestas sollicitudin, mauris eros
-              vulputate odio, interdum pretium mauris lorem sed enim. Sed a
-              maximus eros. &rdquo;
-            </p>
-            <footer className="text-sm">Sofia Davis</footer>
+            <p className="text-lg">&ldquo;Time saved is progress gained—launch your project swiftly with The Startup Template. &rdquo;</p>
+            <footer className="text-sm">Naman Parashar</footer>
           </blockquote>
         </div>
       </div>
       <div className="p-4 lg:p-8 h-full flex items-center">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Enter your email below to create your account
-            </p>
+            <h1 className="text-2xl font-semibold tracking-tight">Create an account</h1>
+            <p className="text-sm text-muted-foreground">Enter your email below to create your account</p>
           </div>
           <div className="grid gap-4">
             <GoogleSignInButton />
@@ -72,17 +44,11 @@ export default function AuthenticationPage() {
           </div>
           <p className="px-8 text-center text-sm text-muted-foreground">
             By clicking continue, you agree to our{" "}
-            <Link
-              href="/terms"
-              className="underline underline-offset-4 hover:text-primary"
-            >
+            <Link href="#" className="underline underline-offset-4 hover:text-primary">
               Terms of Service
             </Link>{" "}
             and{" "}
-            <Link
-              href="/privacy"
-              className="underline underline-offset-4 hover:text-primary"
-            >
+            <Link href="#" className="underline underline-offset-4 hover:text-primary">
               Privacy Policy
             </Link>
             .
