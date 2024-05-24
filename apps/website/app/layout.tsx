90b794e5b74131f99a51aad71103f7bef23a5311
Naman Parashar <nparashar150@gmail.com>
2024-05-25 01:25:20 +0530

    clean(website): remove analytics from template

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -2,7 +2,6 @@ import "@repo/ui/globals.css";
 import type { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import AnalyticsProvider from "./components/AnalyticsProvider";
 import SessionProvider from "./components/SessionProvider";
 import { ThemeProvider } from "./components/ThemeProviders";
@@ -19,19 +18,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
   return (
     <html lang="en">
-      <Script id="gtag" async src="https://www.googletagmanager.com/gtag/js?id=G-6ZHLYHQDD1" />
-      <Script
-        id="gatg-load"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-6ZHLYHQDD1');
-        `,
-        }}
-      />
       <body className={inter.className}>
         <SessionProvider session={session}>
           <AnalyticsProvider email={session?.user?.email} username={session?.user?.name}>
