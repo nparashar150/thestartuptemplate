f472dd8a9edbc81bdb3e985324a47cb2b75773b8
Naman Parashar <nparashar150@gmail.com>
2024-05-24 09:03:13 +0530

    fix(website): track analytics on prod only

--- apps/website/app/components/AnalyticsProvider.tsx
+++ apps/website/app/components/AnalyticsProvider.tsx
@@ -5,7 +5,7 @@ import LogRocket from "logrocket";
 import setupLogRocketReact from "logrocket-react";
 
 const AnalyticsProvider: FC<AnalyticsProviderProps> = ({ children, email, username }) => {
-  if (typeof window !== "undefined") {
+  if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
     LogRocket.init("the-startup-template/thestartuptemplate");
     // plugins should also only be initialized when in the browser
     setupLogRocketReact(LogRocket);
